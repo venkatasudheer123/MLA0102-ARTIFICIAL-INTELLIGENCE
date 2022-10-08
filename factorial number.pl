decdb216f4bde646b079f61dddf25e716f562ea2
venkatasudheer123 <114897645+venkatasudheer123@users.noreply.github.com>
2022-10-08 09:11:25 +0530

    Create factorial number.pl

---
+++ factorial number.pl
@@ -0,0 +1,19 @@
+ predicates
+        factorial(integer, real)
+        go
+
+    clauses
+        go if
+        write("Enter a positive integer number:"),
+        readint(N),
+        factorial(N,Result),
+        write("Factorial of", N, "is=", Result).
+        factorial(0, 1)
+        factorial(N, Result) if N>0,
+                        N1=N-1,
+                        factorial(N1, Res),
+                        Result=N*Res.
+OUTPUT:
+	goals:
+factorial(5, Answer)
+Answer=120
